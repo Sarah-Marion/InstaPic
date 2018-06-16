1c5518fc99afb1aaed49b7d41e8242319ebd2acc
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-16 12:18:44 +0300

    created an image model defined image objects and initialised it

--- insta/models.py
+++ insta/models.py
@@ -34,4 +34,60 @@ class Profile(models.Model):
         return found_profiles
 
 
+        
+class Image(models.Model):
+    """
+    Image class to define Image Objects
+    """
+    image = models.ImageField(upload_to="images/",null = True )
+    image_name = models.CharField(max_length =30,null = True ) 
+    image_caption = models.TextField(null = True )
+    pub_date = models.DateTimeField(auto_now_add=True, null= True)
+    profile_key = models.ForeignKey(Profile,on_delete=models.CASCADE, null = True)
+    user_key = models.ForeignKey(User,on_delete= models.CASCADE , null = True)
+    likes = models.PositiveIntegerField(default=0)
+    comments_number = models.PositiveIntegerField(default=0)
+        
+    def __str__(self):
+        return self.image_name 
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete() 
+
+    def update_caption(self,new_caption):
+        self.image_caption = new_caption
+        self.save()
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        retrived_image = Image.objects.get(id = id)
+        return retrived_image
+
+    @classmethod
+    def get_images_by_user(cls,id):
+        posted_images = Image.objects.filter(user_id=id)
+        return posted_images
+
+    class Meta:
+        """
+        Ordering of posts with the most recent showing from the top most
+        """
+        ordering = ['-pub_date']
+
+    @classmethod
+    def get_all_images(cls):
+        all_posted_images = cls.objects.all()
+        return all_posted_images 
+
+    @classmethod
+    def get_timeline_posts(cls):
+        """
+        A Function to get all posts of people that the current user follows
+        """
+        timeline_posts = Image.objects.filter()
+
+
 
