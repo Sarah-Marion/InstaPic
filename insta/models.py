76ca6b100779fd57c8f0e32575122be8e28d711f
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-16 12:17:35 +0300

    created a profile model defined objects of each profile

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tinymce import HTMLField
+# from tinymce import HTMLField
 from django.db.models import Sum
 # from django.dispatch import receiver
 # from django.db.models.signals import post_save
@@ -8,35 +8,30 @@ from django.db.models import Sum
 # from imagekit.models import ImageSpecField
 
 # Create your models here.
-class Image(models.Model):
+
+class Profile(models.Model):
     """
-    Image class to define Image Objects
+    Profile class that defines objects of each profile
     """
-    image = models.ImageField(upload_to='images/', verbose_name="Select Pic", null=True)
-    image_name = models.CharField(max_length=30, verbose_name='Photo Name', null=True)
-    image_caption = models.TextField(max_length=250, blank=True, verbose_name='Caption', null=True)
-    pub_date = models.DateTimeField(auto_now_add=True, null=True)
-    image_created_by = models.ForeignKey('Profile', vebose_name='Created by', related_name='owner')
-    image_likes = models.ManyToManyField('Profile', related_name='Likes', blank=True, default=False)
-    image_comments = models.ManyToManyField('Profile', through='Comment', through_fields=('comment_owner', 'comment_image') default=False)
-
-
-    class Meta:
-        ordering = ['-pub_date']
-
+    username = models.CharField(max_length=30,default='User')
+    profile_photo = models.ImageField(upload_to="images/",null = True)
+    bio = models.TextField(default='User does Not have a Bio yet',blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null= True )
+    
     def __str__(self):
-        return self.image_name
+        return self.username
 
-    def save_image(self):
+    def save_profile(self):
         self.save()
 
-    def delete_image(self):
+    def delete_profile(self):
         self.delete()
 
 
+    @classmethod
+    def find_profile(cls,name):
+        found_profiles = cls.objects.filter(username__icontains = name).all()
+        return found_profiles
 
 
 
-
-
-    
