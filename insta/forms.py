932728ec04862ab5b0602d38760bb9f2b94cfe10
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-16 12:26:53 +0300

    created a form for commenting on an image

--- insta/forms.py
+++ insta/forms.py
@@ -1,44 +1,30 @@
-from .models import Image
+from .models import Profile, Image
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm #UserCreationForm
 from django.contrib.auth.models import User
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=250, help_text='Required. Kindly input a valid email address' )
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', '1st_password', '2nd_password')
-        unique_together = ('email')
-
-    def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['placeholder'] = 'Username'
-        self.fields['email'].widget.attrs['placeholder'] = 'Email'
-        self.fields['1st_password'].widget.attrs['placeholder'] = 'Password'
-        self.fields['2nd_password'].widget.attrs['placeholder'] = 'Confirm your Password'
-
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=TextInput(attrs={'class':'validate', 'placeholder':'Username'}))
-    password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Password'}))
-
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('email')
-
-
-class NewPostForm(forms.ModelForm):
+class ImageForm(forms.ModelForm): 
+    """
+    Class that creates an Image upload form
+    """
     class Meta:
         model = Image
-        fields = ('image_name', 'image', 'image_caption')
+        fields = ['image','image_name','image_caption']
 
 
-    def __init__(self, *args, **kwargs):
-        super(NewPostForm, self).__init__(*args, **kwargs)
-        self.fields['image_name'].widget.attrs['placeholder'] = 'Kindly input an image name'
-        self.fields['image'].widget.attrs['placeholder'] = 'Pick a Pic'
-        self.fields['image_caption'].widget.attrs['placeholder'] = 'Kindly add a Caption to the image'
+class ProfileUpdateForm(forms.Form):
+    """
+    classs that creates a Profile update form
+    """ 
+    username = forms.CharField(label='Username', max_length = 30)
+    profile_photo = forms.ImageField(label = 'Image Field') 
+    bio = forms.CharField(label='Image Caption', max_length=500)
+
+class CommentForm(forms.ModelForm):
+   """
+    class that creates the comment form
+   """
+    class Meta:
+        model = Comment
+        fields = ['comment']
